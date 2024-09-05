e3f98929da67e81a7df96b832de91caab83d1db6
Arseniy Shatalov <56681990+arsvrn@users.noreply.github.com>
2024-09-05 13:23:39 +0300

    Домашняя работа #2 (#2)
    * Домашняя работа #2
    
    1. Подключены внешние зависимости Lombok и Jackson
    2. Созданы классы на основе json: City и Coords
    3. Реализованы методы readJson, toXML, saveToFile
    4. Использовано логирование @Slf4j. Уровни info, debug, error
    5. Написаны unit тесты
    
    * Bugfix

---
+++ src/main/java/com/tbank/processors/DataProcessor.java
@@ -0,0 +1,67 @@
+package com.tbank.processors;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Slf4j
+@AllArgsConstructor
+public class DataProcessor<T> {
+
+    private final ObjectMapper objectMapper;
+    private final XmlMapper xmlMapper;
+
+    public DataProcessor() {
+        this.objectMapper = new ObjectMapper();
+        this.xmlMapper = new XmlMapper();
+    }
+
+    public T readJson(String filePath, Class<T> clazz) throws IOException {
+        log.info("Starting to read JSON from a file: {}", filePath);
+        try {
+            T result = objectMapper.readValue(new File(filePath), clazz);
+            log.debug("JSON has been successfully read and an object has been created: {}", result);
+            return result;
+        } catch (JsonProcessingException e) {
+            log.error("JSON parsing error: {}", e.getMessage());
+            throw e;
+        } catch (IOException e) {
+            log.error("File reading error: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    public String toXML(T object) throws JsonProcessingException {
+        log.info("Converting an object to XML");
+        try {
+            String xml = xmlMapper.writeValueAsString(object);
+            log.debug("The conversion is successful: {}", xml);
+            return xml;
+        } catch (JsonProcessingException e) {
+            log.error("Error converting to XML: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    public void saveToFile(String data, String filePath) throws IOException {
+        log.info("Saving data to a file: {}", filePath);
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                boolean created = file.createNewFile();
+                log.debug("The file has been created: {}", created);
+            }
+            Files.writeString(file.toPath(), data);
+            log.info("The data has been successfully saved to a file: {}", filePath);
+        } catch (IOException e) {
+            log.error("Error saving to file: {}", e.getMessage());
+            throw e;
+        }
+    }
+}
